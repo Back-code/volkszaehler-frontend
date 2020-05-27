b1ba6c5bbefc6240297987510ffed34fad4c474c
thgau <thgau@users.noreply.github.com>
2020-05-27 22:40:57 +0200

    adopt startit.sh

--- vz/startit.sh
+++ vz/startit.sh
@@ -4,15 +4,11 @@
 
 cd /var/www/volkszaehler.org/etc
 cp config.dist.yaml config.yaml
-sed -i -e "s/host: 127.0.0.1/host: ${db_host}/" -e "s/# port: 3306/port: ${db_port}/" -e "s/user: vz/user: ${db_user}/" -e "s/password: demo/password: ${db_pass}/" config.yaml
+sed -i -e "s/host: localhost/host: ${db_host}/" -e "s/# port: 3306/port: ${db_port}/" -e "s/user: vz/user: ${db_user}/" -e "s/password: demo/password: ${db_pass}/" config.yaml
 
-cd  /var/www/volkszaehler.org/etc
-sed -i s,htdocs,/var/www/volkszaehler.org/htdocs, middleware.json
-# seems to run twice, delete double replacement
-sed -i s,volkszaehler.org//var/www/,,  middleware.json
 
 cd  /var/www/volkszaehler.org/htdocs
-sed -i s/api// js/options.js
+sed -i s,api,., js/options.js
 
 cd /var/www/volkszaehler.org
 /usr/bin/php /var/www/volkszaehler.org/vendor/bin/ppm start -c /var/www/volkszaehler.org/etc/middleware.json --cgi-path=/usr/bin/php
